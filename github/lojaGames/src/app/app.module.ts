2c384e29ef275504e661e9ca74c7b4ddb709bf80
Mateus Vieira <mateusmvsud@hotmail.com>
2024-08-05 10:30:21 -0300

    adicionando db.json

--- github/lojaGames/src/app/app.module.ts
+++ github/lojaGames/src/app/app.module.ts
@@ -14,15 +14,17 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
+
+/* Importações de Componentes */
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 
 const routes: Routes = [
-  {path:'inicio', component: InicioComponent},
   {path:'login', component: LoginComponent},
-  {path:'', redirectTo:'/inicio',pathMatch:'full'}
+  {path: 'inicio', component: InicioComponent},
+  {path: '', component: InicioComponent}
 ];
 
 @NgModule({
